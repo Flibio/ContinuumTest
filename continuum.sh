f0a6a2a814dec78515c48657c584c0931f439685
Flibio <flibio.business@gmail.com>
2016-07-30 20:59:15 -0600

    try again

--- continuum.sh
+++ continuum.sh
@@ -12,10 +12,10 @@ chmod +x ./jq
 sudo cp /usr/bin
 
 # Create a new Continuum build #
-RESPONSE=$(curl -v -X POST -d "project=${PROJECT}&commit=${TRAVIS_COMMIT}&job=${TRAVIS_JOB_ID}" -u continuum:${CONTINUUM-TOKEN} "${TARGET}newbuild.php")
+RESPONSE=$(curl -v -X POST -d "project=${PROJECT}&commit=${TRAVIS_COMMIT}&job=${TRAVIS_JOB_ID}" -u continuum:${CONTINUUM_TOKEN} "${TARGET}newbuild.php")
 echo ${RESPONSE}
-BUILD=$(${RESPONSE} | jq '.build');
-STATUS=$(${RESPONSE} | jq '.status');
+BUILD=$("${RESPONSE}" | jq '.build');
+STATUS=$("${RESPONSE}" | jq '.status');
 
 echo "New build status: ${STATUS}"
 
